2a1aca06b96acec7a0953732ae015641e275b5e0
Aman Talreja <64047297+amantalreja@users.noreply.github.com>
2023-05-29 18:02:06 +0000

    first commit

---
+++ calculator/src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display"></div>
+        <div className="operators">
+          <button>+</button>
+          <button>-</button>
+          <button>/</button>
+          <button>*</button>
+        </div>
+        <div className="digits">
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
