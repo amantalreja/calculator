b81988897d269a00c11d07583fe764f6ff39bc3d
Aman Talreja <64047297+amantalreja@users.noreply.github.com>
2023-05-29 19:20:37 +0000

    second

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,23 +1,26 @@
 function App() {
+  function digitCreater(){
+    var digits= [0,1,4,5,6,7,8,9]
+    digits= digits.map((number) =>
+    <button key={number}>{number}</button>
+  );
+  return digits;
+  }
   return (
     <div className="App">
-      <div className="calculator">
-        <div className="display"></div>
-        <div className="operators">
+      <div className="Calculator">
+        <div className="Display"></div>
+        <div className="Operators">
           <button>+</button>
           <button>-</button>
           <button>/</button>
           <button>*</button>
+          <button>Del</button>
         </div>
         <div className="digits">
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+         {digitCreater()}
+         <button>.</button>
+         <button>=</button>
         </div>
       </div>
     </div>
